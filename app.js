400287a71e41c300dbc0b0e1ca05962098fb2e5a
Fidel Sosa <fidel@fidelsosa.com>
2013-11-24 20:43:17 -0500

    Added mysql dialect for Sequelize. Created analytics ER model for database. Tested db connection

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 var express = require('express')
+var Sequelize = require('sequelize')
+
 var app = express();
 
+// NOTE:
+// For dev, uncomment 'skip-networking' in XAMPP_DIR/etc/my.cnf
+// Or update sequelize with socket-passthrough
+var sequelize = new Sequelize('test', 'root', 'itllbeok', {
+	host:'localhost',
+	dialect: 'mysql'
+})
+
 app.get('/', function(req, res) {
-	res.send('It works!');
+	sequelize.query('SELECT * FROM data').success(function(rows) {
+		res.send(rows);
+	}).failure(function(err) {
+		res.send(err);
+	})
+	
 });
 
 app.listen(3000);
