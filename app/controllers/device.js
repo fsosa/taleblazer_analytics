39ea24093101335af160bcd57c24c74ddaf9e17d
Fidel Sosa <fidel@fidelsosa.com>
2014-03-18 23:58:37 -0400

    experimenting with how to get the data for the overview page

--- app/controllers/device.js
+++ app/controllers/device.js
@@ -1,14 +1,36 @@
 var db = require('../models');
+var _  = require('underscore');
 
 
 exports.index = function(req, res) {
-	db.Device.findAll()
-		.success(function(devices) {
-			res.jsend(devices);
+	// db.Device.findAll()
+	// 	.success(function(devices) {
+	// 		res.jsend(devices);
+	// 	})
+	// 	.error(function(error) {
+	// 		res.jerror(500, error);
+	// 	});
+	// 	
+	// 		select * from sessions where (started_at between '2014-01-14' and '2014-03-20') and draft_state_id IN (select id from draft_states where draft_id = 1 and published_game = 1); 
+	// 		
+	db.DraftState
+		.findAll({ where: { draft_id: 4, published_game: 1 }, attributes: ['id'] })
+		.success(function(resu) {
+			var ids = _.map(resu, function(result) { return result.values['id'] });
+			console.log(ids);
+			db.Session
+				.findAndCountAll({ where: { started_at: { between: ['2014-01-14', '2015-01-01'] }, draft_state_id: ids } })
+				.success(function(results) {
+					res.jsend(results);
+				})
+				.error(function(error){
+					res.jerror(500, error);	
+				})							
 		})
 		.error(function(error) {
 			res.jerror(500, error);
-		});
+		})
+
 };
 
 exports.create = function(req, res) {
