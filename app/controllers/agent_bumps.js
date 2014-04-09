ee9bdeaf315a3f50b3236a65396d3df218df961a
Fidel Sosa <fidel@fidelsosa.com>
2014-04-09 16:39:00 -0400

    experimenting with big csv

--- app/controllers/agent_bumps.js
+++ app/controllers/agent_bumps.js
@@ -11,31 +11,123 @@ var CATEGORIZE_TYPE = {
 };
 
 exports.show = function(req, res, next) {
-	var draft_id = req.params.draft_id;
-
-	// Render the page if it's not an AJAX request 
-	// TODO: or JSON/CSV
-	if (!req.xhr) {
-		res.render('agent-bumps.ect', {
-			draft_id: draft_id,
-			title: 'Agent Bumps', 
-			script: 'overview.js'
+	db.AgentBump
+		.findAll({
+			include: [{
+				model: db.Session,
+				where: {
+					draft_state_id: 169
+				}
+			}]
 		})
+		.success(function(agentBumps) {
+			db.RegionSwitch
+				.findAll({
+					include: [{
+						model: db.Session,
+						where: {
+							draft_state_id: 169
+						}
+					}]
+				})
+				.success(function(regionSwitches) {
+					var results = [];
 
-		return;
-	}
-
-	// Otherwise we start processing the API call
-	var start_time = req.query.start_time;
-	var end_time = req.query.end_time;
-	var categorize_by = req.query.categorize_by;
-	// TODO: ADD TYPE options (JSON or CSV)
-	
-
-	if (start_time == null || end_time == null) {
-		res.jerror(400, 'start_time and end_time parameters are required');
-		return;
-	}
+					var sessionFields = Object.keys(agentBumps[0].values.session.values);
+					var agentBumpFields = Object.keys(agentBumps[0].values);
+					var regionFields = Object.keys(regionSwitches[0].values);
+					var defaults = {};
 
 
-};
+					var fields = [];
+					fields.push(sessionFields);
+					fields.push(agentBumpFields);
+					fields.push(regionFields);
+					fields = _.flatten(fields);
+					results.push(fields);
+
+					_.each(agentBumpFields, function(field) {
+						defaults[field] = null;
+					})
+
+					_.each(regionFields, function(field) {
+						defaults[field] = null;
+					})
+
+					_.each(agentBumps, function(agentBump) {
+						var finalresults = {};
+						var rawSession = agentBump.values.session.values;
+
+						_.each(rawSession, function(val, key) {
+							finalresults[key] = val;
+						});
+
+						var raw = agentBump.values;
+						var modified = _.defaults(raw, defaults);
+						delete modified.session;
+
+						_.extend(finalresults, modified);
+
+						console.log(finalresults);
+
+						results.push(finalresults);
+					});
+
+					_.each(regionSwitches, function(agentBump) {
+						var finalresults = {};
+						var rawSession = agentBump.values.session.values;
+
+						_.each(rawSession, function(val, key) {
+							finalresults[key] = val;
+						});
+
+						var raw = agentBump.values;
+						var modified = _.defaults(raw, defaults);
+						delete modified.session;
+
+						_.extend(finalresults, modified);
+
+						results.push(finalresults);
+					});
+
+					// res.jsend(results);
+					res.csv(results);
+				})
+				.error(function(error) {
+					next(error);
+				});
+		})
+		.error(function(error) {
+			next(error);
+		})
+}
+
+// exports.show = function(req, res, next) {
+// 	var draft_id = req.params.draft_id;
+
+// 	// Render the page if it's not an AJAX request 
+// 	// TODO: or JSON/CSV
+// 	if (!req.xhr) {
+// 		res.render('agent-bumps.ect', {
+// 			draft_id: draft_id,
+// 			title: 'Agent Bumps', 
+// 			script: 'overview.js'
+// 		})
+
+// 		return;
+// 	}
+
+// 	// Otherwise we start processing the API call
+// 	var start_time = req.query.start_time;
+// 	var end_time = req.query.end_time;
+// 	var categorize_by = req.query.categorize_by;
+// 	// TODO: ADD TYPE options (JSON or CSV)
+
+
+// 	if (start_time == null || end_time == null) {
+// 		res.jerror(400, 'start_time and end_time parameters are required');
+// 		return;
+// 	}
+
+
+// };
