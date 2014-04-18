08b6caf88c4e8b68d23050129054d7ee325b62fa
Fidel Sosa <fidel@fidelsosa.com>
2014-04-18 16:07:50 -0400

    Clarified what the config.HOST option does

--- config/config.js
+++ config/config.js
@@ -15,13 +15,13 @@ module.exports = {
     LOG_DIR: './logs/'
   },
   test: {
-    db: dbConfig.test,  // Remove this or set as empty string to have Node serve requests to any IP
+    db: dbConfig.test,  // Remove this or set as empty string to have Node serve requests to any IP (useful for letting external machines connect to your local server instance)
     PORT: 3000,
     LOG_DIR: './logs/'
   },
   production: {
     db: dbConfig.production, 
-    HOST: '127.0.0.1', // Remove this or set as empty string to have Node serve requests to any IP
+    HOST: '127.0.0.1', // Remove this or set as empty string to have Node serve requests to any IP (useful for letting external machines connect to your local server instance)
     PORT: 3000,
     LOG_DIR: './logs/'
   }
